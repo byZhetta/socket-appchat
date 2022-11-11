e385ec6e569adb6410c3d70d10f08d54427fde3d
ignaciozhetta <zagu4988@gmail.com>
2022-11-11 12:30:24 -0300

    chat ended v1

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Notas:
+# Notas Socket Chat:
 
 Este es un pequeño servidor de express listo para ejecutarse y servir la carpeta public en la web.
 
--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -12,11 +12,13 @@ var usuario = {
     sala: params.get('sala')
 };
 
+
 socket.on('connect', function() {
     console.log('Conectado al servidor');
 
-    socket.emit('entrarChat',  usuario, function(resp) {
-        console.log('Usuarios conectados', resp);
+    socket.emit('entrarChat', usuario, function(resp) {
+        // console.log('Usuarios conectados', resp);
+        renderizarUsuarios(resp);
     });
 
 });
@@ -39,17 +41,21 @@ socket.on('disconnect', function() {
 
 // Escuchar información
 socket.on('crearMensaje', function(mensaje) {
-
-    console.log('Servidor:', mensaje);
-
+    // console.log('Servidor:', mensaje);
+    renderizarMensajes(mensaje, false);
+    scrollBottom();
 });
 
-// Escuchar cambios de usuarios (cuando un usuario entra o sale del chat)
+// Escuchar cambios de usuarios
+// cuando un usuario entra o sale del chat
 socket.on('listaPersona', function(personas) {
-    console.log(personas);
+    // console.log(personas);
+    renderizarUsuarios(personas);
 });
 
 // Mensajes privados
 socket.on('mensajePrivado', function(mensaje) {
+
     console.log('Mensaje Privado:', mensaje);
+
 });
